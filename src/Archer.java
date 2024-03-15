14009d635fc40f265c3ec0448b43de1fb7015a5e
Smegafoo <emiruyar2002@gmail.com>
2024-03-15 15:32:40 +0300

    Karakter sınıfı oluşturuldu Archer eklendi

---
+++ src/Archer.java
@@ -0,0 +1,5 @@
+public class Archer extends Character{
+    Archer(){
+        super("Archer",15,20,50);
+    }
+}
---
+++ src/Character.java
@@ -0,0 +1,13 @@
+public class Character {
+    private String name;
+    private int damage;
+    private int hp;
+    private int money;
+
+    public Character(String name, int damage, int hp, int money) {
+        this.name = name;
+        this.damage = damage;
+        this.hp = hp;
+        this.money = money;
+    }
+}
