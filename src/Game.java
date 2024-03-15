12d585430402bd3f3f9855404231532766edd607
Smegafoo <emiruyar2002@gmail.com>
2024-03-15 15:48:15 +0300

    Karakter sınıfı Game içerisine alındı ve karakter özellikleri görüntülendi

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,18 @@
+import java.util.Arrays;
+
 public class Game {
     public static void main(String[] args) {
+        Character c1;
 
     }
     public void Start_game(){
+        Character Character[]={new Archer()};
         System.out.println("Please select your Character Type ");
+        for (Character mc1: Arrays.asList(Character)) {
+            System.out.println("Character name:"+ mc1.getName()+
+                    "\nCharacter damage:"+mc1.getDamage()+"\nCharacter HP:"+mc1.getHp()+"\nCharacter Money:"+mc1.getMoney());
+
+        }
 
     }
 
